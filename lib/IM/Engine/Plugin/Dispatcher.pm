d84cfe8bcf7e95eb211366ec9045133ba31f89f3
Shawn M Moore <sartak@gmail.com>
2009-06-27 15:18:18 -0400

    Load up a dispatcher class

--- lib/IM/Engine/Plugin/Dispatcher.pm
+++ lib/IM/Engine/Plugin/Dispatcher.pm
@@ -1,10 +1,33 @@
 package IM::Engine::Plugin::Dispatcher;
 use Moose;
+use Moose::Util::TypeConstraints;
 extends 'IM::Engine::Plugin';
+
+subtype 'IM::Engine::Plugin::Dispatcher::Dispatcher'
+     => as 'Path::Dispatcher';
+
+coerce 'IM::Engine::Plugin::Dispatcher::Dispatcher'
+    => from 'Str'
+    => via {
+        my $class = $_;
+        Class::MOP::load_class($class);
+
+        # A subclass of Path::Dispatcher
+        if ($class->can('new')) {
+            return $class->new;
+        }
+        # A sybclass of Path::Dispatcher::Declarative
+        else {
+            return $class->dispatcher;
+        }
+
+        # would be nice to improve this...
+    };
 
 has dispatcher => (
     is       => 'ro',
-    isa      => 'ClassName',
+    isa      => 'IM::Engine::Plugin::Dispatcher::Dispatcher',
+    coerce   => 1,
     required => 1,
 );
 
